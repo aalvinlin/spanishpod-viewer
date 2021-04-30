fdf65e367ca08f429aa886d37ee63caa9cb39e14
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-04-29 19:36:44 -0700

    add static server to serve CSS files

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ server.use(express.json());
 
 server.set("view engine", "pug");
 
+server.use(express.static("public"));
+
 server.use("/", (req, res) => {
 
     let directoriesAndFiles = getFilesAndDirectories("./files");
